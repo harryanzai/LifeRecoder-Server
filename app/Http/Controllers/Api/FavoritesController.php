1fd30941d3bf5e246c0ae1174b3a4e4e059c2c9e
wowiwj <35649084@qq.com>
2017-05-07 17:17:24 +0800

    更新文档

--- app/Http/Controllers/Api/FavoritesController.php
+++ app/Http/Controllers/Api/FavoritesController.php
@@ -26,6 +26,29 @@ class FavoritesController extends ApiController
         'comment.favorite' => Comment::class
     ];
 
+    /**
+     * @api {post} /v1/galleries/:gallery/favorites 添加喜欢的图集
+     * @apiGroup Favorite
+     * @apiHeaderExample {json} Header-Example:
+     *    {
+     *       "Authorization" : "Bearer {token}"
+     *    }
+     * @apiPermission none
+     * @apiParam {int} gallery 对应图片集的id
+     * @apiVersion 0.0.0
+     * @apiSuccessExample {json} Success-Response:
+     *     {
+     *       "status": "success",
+     *       "code": 200,
+     *       "message": "添加成功"
+     *      }
+     * @apiErrorExample {json} Error-Response:
+     *   {
+     *    "status": "error",
+     *    "code": 404,
+     *    "message": "没有找到该资源"
+     *   }
+     */
     public function store(Request $request,$id)
     {
 
@@ -36,6 +59,29 @@ class FavoritesController extends ApiController
         return $this->respondWithMessage('添加成功');
     }
 
+    /**
+     * @api {delete} /v1/galleries/:gallery/favorites 取消喜欢的图集
+     * @apiGroup Favorite
+     * @apiHeaderExample {json} Header-Example:
+     *    {
+     *       "Authorization" : "Bearer {token}"
+     *    }
+     * @apiPermission none
+     * @apiParam {int} gallery 对应图片集的id
+     * @apiVersion 0.0.0
+     * @apiSuccessExample {json} Success-Response:
+     *     {
+     *       "status": "success",
+     *       "code": 200,
+     *       "message": "取消喜欢成功"
+     *      }
+     * @apiErrorExample {json} Error-Response:
+     *   {
+     *    "status": "error",
+     *    "code": 404,
+     *    "message": "没有找到该资源"
+     *   }
+     */
     public function destroy(Request $request,$id)
     {
         $modelClass = $this->models[$request->route()->getName()];
