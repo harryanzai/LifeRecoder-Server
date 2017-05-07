30f195bf501f610c4b44edf40dfa068060eab18a
wowiwj <35649084@qq.com>
2017-05-07 19:40:46 +0800

    添加用户的关注与被关注功能

--- app/Models/User.php
+++ app/Models/User.php
@@ -35,11 +35,60 @@ class User extends Authenticatable
 
     }
 
+    /**
+     * 一个用户可以有很多评论
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
     public function comments()
     {
         return $this->hasMany(Comment::class);
     }
 
+    /**
+     * 一个用户可以有很多关注者
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function followers()
+    {
+        return $this->belongsToMany(User::class,'followers','user_id','follower_id');
+
+    }
+
+    /**
+     * 一个用户可以关注很多人
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function followings()
+    {
+        return $this->belongsToMany(User::class,'followers','follower_id','user_id');
+
+    }
+
+
+
+    public function follow($user_ids)
+    {
+        if (!is_array($user_ids)){
+            $user_ids = compact('user_ids');
+        }
+
+        $this->followings()->sync($user_ids,false);
+
+    }
+
+    public function unfollow($user_ids)
+    {
+        if (!is_array($user_ids)){
+            $user_ids = compact('user_ids');
+        }
+
+        $this->followings()->detach($user_ids,false);
+    }
+
+    public function isFollowing($user_id)
+    {
+        return $this->followings->contains($user_id);
+    }
 
 
     /**
